b9630640088c9e195fc4506ffc9740b75f3388b8
Fdiannz <felysiazoldyck04@gmail.com>
2025-05-30 00:36:01 +0800

    create react app

---
+++ ShelfAware/src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CSS/Footer.css"
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <h1 className='title'>Footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
