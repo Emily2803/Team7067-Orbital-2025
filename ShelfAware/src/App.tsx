44a465c37666afab5b4637d54a0bad9bfe2f8710
Fdiannz <felysiazoldyck04@gmail.com>
2025-05-30 00:36:01 +0800

    create react app

---
+++ ShelfAware/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CSS/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>Shelf Aware</h1>
+    </div>
+  );
+}
+
+export default App;
