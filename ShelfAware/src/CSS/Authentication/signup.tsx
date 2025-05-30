6114622fe2f6073da0c3eb441b8f7929b0662ba3
Fdiannz <felysiazoldyck04@gmail.com>
2025-05-30 23:59:52 +0800

    change layout

--- ShelfAware/src/CSS/Authentication/signup.tsx
+++ ShelfAware/src/CSS/Authentication/signup.tsx
@@ -1,74 +1,81 @@
 import { auth } from "../../firebase";
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import './Auth.css'
+import './Auth.css';
 import { useNavigate } from 'react-router-dom';
 
 function UserSignUp() {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const[fname, setFname] = useState("");
-    const[lname, setLname] = useState("");
+    const [fname, setFname] = useState("");
+    const [lname, setLname] = useState("");
 
-    const handleRegister = async(e: React.FormEvent) => {
+    const handleRegister = async (e: React.FormEvent) => {
         e.preventDefault();
+
         try {
-            await createUserWithEmailAndPassword(auth, email, password);
-            alert("Signed up!")
-        } catch (error:any) {
-            console.log(error.message);
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log("User signed up:", userCredential.user);
+            alert("Signed up!");
+            setTimeout(() => {
+                navigate("/");
+            }, 100);
+        } catch (error: any) {
+            console.error("Signup error:", error.message);
+            alert("Signup failed: " + error.message);
         }
     };
-    return(
-        <div className = "authPopUp">
-         <form onSubmit={handleRegister}>
-          <button
-           className = "closePopUp"
-           type = "button"
-           onClick = {() => navigate("/")}
-           >
-            x
-          </button>
-            <h2>Sign Up</h2>
-            <br/>
-            
-                <input
-                 type = "text"
-                 placeholder = "First Name"
-                 onChange = {(e) => setFname(e.target.value)}
-                 required
-                />
-                <br/>
+
+    return (
+        <div className="authPopUp">
+            <form onSubmit={handleRegister}>
+                <button
+                    className="closePopUp"
+                    type="button"
+                    onClick={() => navigate("/")}
+                >
+                    ×
+                </button>
+                <h2>Sign Up</h2>
 
                 <input
-                 type = "text"
-                 placeholder = "Last Name"
-                 onChange = {(e) => setLname(e.target.value)}
-                 required
+                    type="text"
+                    placeholder="First Name"
+                    value={fname}
+                    onChange={(e) => setFname(e.target.value)}
+                    required
                 />
-                <br/>
 
-                <input 
-                 type ="email"
-                 placeholder = "Email"
-                 onChange={(e) => setEmail(e.target.value)}
-                 required
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    value={lname}
+                    onChange={(e) => setLname(e.target.value)}
+                    required
                 />
-                <br/>
-            
-                <input 
-                 type = "password"
-                 placeholder = "Password"
-                 onChange = {(e) => setPassword(e.target.value)}
-                 required 
-                />
-                <br/>
 
-                <button type = "submit">Sign Up</button>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">Sign Up</button>
             </form>
         </div>
     );
 }
 
 export default UserSignUp;
+
