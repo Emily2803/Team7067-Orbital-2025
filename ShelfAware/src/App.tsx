180184b62403c525c4ac4f618ebcb9d4c2ff02d9
Emily2803 <e1356704@u.nus.edu>
2025-05-31 12:40:36 +0800

    added a pantry page

--- ShelfAware/src/App.tsx
+++ ShelfAware/src/App.tsx
@@ -5,6 +5,7 @@ import UserSignUp from "./CSS/Authentication/signup";
 import UserLogin from "./CSS/Authentication/login"; 
 import Home from './Home';
 import Landing from './Landing';
+import PantryPage from "./CSS/Pantry/PantryPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signup" element={<UserSignUp />} />
           <Route path="/login" element={<UserLogin />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/pantry" element={<PantryPage />} />
         </Routes>
       </div>
     </Router>
